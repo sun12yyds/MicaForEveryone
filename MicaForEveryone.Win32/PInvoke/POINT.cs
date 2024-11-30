f37513cb65f3440822881df8f4eab37560075a9b
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 19:51:37 +0800

    Add files via upload

---
+++ MicaForEveryone.Win32/PInvoke/POINT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MicaForEveryone.Win32.PInvoke
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
