d2b22870df1156b2bf24a28053b4b9209e28092c
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 21:35:34 +0800

    Add files via upload

---
+++ MicaForEveryone.UI/Triggers/GeneralPaneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using MicaForEveryone.UI.Models;
+using MicaForEveryone.Core.Ui.Interfaces;
+using MicaForEveryone.Core.Ui.Models;
+
+namespace MicaForEveryone.UI.Triggers
+{
+    public class GeneralPaneTrigger : StateTriggerBase
+    {
+        private IPaneItem _paneItem;
+
+        public IPaneItem PaneItem
+        {
+            get => _paneItem;
+            set
+            {
+                _paneItem = value;
+                SetActive(_paneItem?.ItemType == PaneItemType.General);
+            }
+        }
+    }
+}
---
+++ MicaForEveryone.UI/Triggers/RulePaneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using MicaForEveryone.UI.Models;
+using MicaForEveryone.Core.Ui.Interfaces;
+using MicaForEveryone.Core.Ui.Models;
+
+namespace MicaForEveryone.UI.Triggers
+{
+    public class RulePaneTrigger : StateTriggerBase
+    {
+        private IPaneItem _paneItem;
+
+        public IPaneItem PaneItem
+        {
+            get => _paneItem;
+            set
+            {
+                _paneItem = value;
+                SetActive(_paneItem != null &&
+                    _paneItem.ItemType != PaneItemType.General);
+            }
+        }
+    }
+}
