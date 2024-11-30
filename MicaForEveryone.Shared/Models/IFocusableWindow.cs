4553652b321316d9f4ee965a1fe046d80fecd805
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 21:39:44 +0800

    Add files via upload

---
+++ MicaForEveryone.Shared/Models/IFocusableWindow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MicaForEveryone.Models
+{
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IFocusableWindow
+    {
+        event EventHandler GotFocus;
+        event EventHandler LostFocus;
+    }
+}
