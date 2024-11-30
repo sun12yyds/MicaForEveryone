dede310135ddd17705c213d6f0774b98a4bc1645
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 21:21:40 +0800

    Add files via upload

---
+++ MicaForEveryone.UI/Selectors/CornerPreferenceSelector.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using MicaForEveryone.Core.Models;
+
+namespace MicaForEveryone.UI.Selectors
+{
+    internal class CornerPreferenceSelector : DataTemplateSelector
+    {
+        public DataTemplate Default { get; set; }
+        public DataTemplate Square { get; set; }
+        public DataTemplate Rounded { get; set; }
+        public DataTemplate RoundedSmall { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item) =>
+            item switch
+            {
+                CornerPreference.Default => Default,
+                CornerPreference.Square => Square,
+                CornerPreference.Rounded => Rounded,
+                CornerPreference.RoundedSmall => RoundedSmall,
+                _ => null
+            };
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return SelectTemplateCore(item);
+        }
+    }
+}
---
+++ MicaForEveryone.UI/Selectors/TitlebarColorSelector.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using MicaForEveryone.Core.Models;
+
+namespace MicaForEveryone.UI.Selectors
+{
+    internal class TitlebarColorSelector : DataTemplateSelector
+    {
+        public DataTemplate Default { get; set; }
+        public DataTemplate System { get; set; }
+        public DataTemplate Light { get; set; }
+        public DataTemplate Dark { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item) =>
+            item switch
+            {
+                TitlebarColorMode.Default => Default,
+                TitlebarColorMode.System => System,
+                TitlebarColorMode.Light => Light,
+                TitlebarColorMode.Dark => Dark,
+                _ => null
+            };
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return SelectTemplateCore(item);
+        }
+    }
+}
