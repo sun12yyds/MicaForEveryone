9f4e6fc59a4205ca69a340d1dc7d7dd440199f06
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 21:41:54 +0800

    Add files via upload

---
+++ MicaForEveryone.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("MicaForEveryone.Shared")]
+[assembly: AssemblyProduct("Mica For Everyone UWP/.NET Interface Library")]
+[assembly: AssemblyCompany("Mica For Everyone")]
+[assembly: AssemblyCopyright("Copyright (C) 2021-2022 Mohammad Amin Mollazadeh and Contributers")]
