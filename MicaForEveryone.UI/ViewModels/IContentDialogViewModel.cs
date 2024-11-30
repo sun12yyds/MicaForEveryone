bb7eb81daf05c0cdde1bf61c953a2098497fe4f0
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-30 21:37:01 +0800

    Add files via upload

---
+++ MicaForEveryone.UI/ViewModels/IContentDialogViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MicaForEveryone.UI.ViewModels
+{
+    public interface IContentDialogViewModel : INotifyPropertyChanged
+    {
+        object Title { get; set; }
+
+        object Content { get; set; }
+
+        bool IsPrimaryButtonEnabled { get; set; }
+
+        object PrimaryButtonContent { get; set; }
+
+        bool IsSecondaryButtonEnabled { get; set; }
+
+        object SecondaryButtonContent { get; set; }
+
+        IRelayCommand PrimaryCommand { get; set; }
+
+        object PrimaryCommandParameter { get; set; }
+
+        IRelayCommand SecondaryCommand { get; set; }
+
+        object SecondaryCommandParameter { get; set; }
+    }
+}
